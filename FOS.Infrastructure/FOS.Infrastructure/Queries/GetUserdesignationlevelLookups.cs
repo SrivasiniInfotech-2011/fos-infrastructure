16348d1b4de85eae6875e5bfe393e13f477be5c4
karthikrossi46 <karthik.n@ascentya.in>
2024-11-29 14:43:57 +0530

    userlevel lookup api created

---
+++ FOS.Infrastructure/FOS.Infrastructure/Queries/GetUserdesignationlevelLookups.cs
@@ -0,0 +1,33 @@
+﻿using FOS.Models.Entities;
+using FOS.Repository.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Infrastructure.Queries
+{
+    public class GetUserdesignationlevelLookups
+    {
+
+        public class Query : IRequest<List<Lookup>>
+        {
+        }
+
+        public class Handler : IRequestHandler<Query, List<Lookup>>
+        {
+            private readonly IUsermanagementRepository _usermanagementRepository;
+
+            public Handler(IUsermanagementRepository repository)
+            {
+                _usermanagementRepository = repository;
+            }
+            public async Task<List<Lookup>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _usermanagementRepository.GetUserdesignationlevelLookup();
+            }
+        }
+    }
+}
---
+++ FOS.Infrastructure/FOS.Infrastructure/Queries/UserLevelLookup.cs
@@ -0,0 +1,42 @@
+﻿using FOS.Models.Entities;
+using FOS.Repository.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Infrastructure.Queries
+{
+    public class UserLevelLookup
+    {
+        public class Query : IRequest<List<Lookup>>
+        {
+            public int? CompanyId { get; }
+            public int? UserId { get; }
+
+
+            public Query(int? userId, int? companyId)
+            {
+                UserId = userId;
+                CompanyId = companyId;
+             
+            }
+        }
+
+        public class Handler : IRequestHandler<Query, List<Lookup>>
+        {
+            private readonly IUsermanagementRepository _UsermanagementRepository;
+
+            public Handler(IUsermanagementRepository repository)
+            {
+                _UsermanagementRepository = repository;
+            }
+            public async Task<List<Lookup>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _UsermanagementRepository.GetUserLevelLookup(request.CompanyId, request.UserId);
+            }
+        }
+    }
+}
