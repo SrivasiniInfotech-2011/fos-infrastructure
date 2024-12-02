49ace65afb3ccd64a7c024f41973bf251353a058
karthikrossi46 <karthik.n@ascentya.in>
2024-12-02 23:11:53 +0530

    User Insert Api changes in repository and Models

--- FOS.Infrastructure/FOS.Infrastructure/Commands/UserdetailsInsert.cs
+++ FOS.Infrastructure/FOS.Infrastructure/Commands/UserdetailsInsert.cs
@@ -1,9 +1,11 @@
-﻿using FOS.Models.Requests;
+﻿using FOS.Models.Entities;
+using FOS.Models.Requests;
 using FOS.Repository.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,32 +17,57 @@ namespace FOS.Infrastructure.Commands
     {
         public class Command : IRequest<int>
         {
-            public UsercDetailsRequestModel UserdetailsCommand { get; set; }
-            public Command(UsercDetailsRequestModel newUserdetails)
+            // here give model name
+            public UserInsertDetailsModel UserdetailsCommand { get; set; }
+            public Command(UserInsertDetailsModel newUserdetails)
             {
                 UserdetailsCommand = newUserdetails;
             }
         }
 
 
-        //public class Handler : IRequestHandler<Command, int>
-        //{
-        //    private readonly IUsermanagementRepository _usermanagementRepository;
-        //    public Handler(IUsermanagementRepository repository)
-        //    {
-        //        _usermanagementRepository = repository;
-        //    }
+        public class Handler : IRequestHandler<Command, int>
+        {
 
-        //    public Task<int> Handle(Command request, CancellationToken cancellationToken)
-        //    {
-        //        int errorCode = 0;
-        //        return _usermanagementRepository.InsertUserDetails(
-        //                                  request.UserdetailsCommand.UserInsertDetails.CompanyId.GetValueOrDefault(), int User_ID, string UserCode, string UserName, int ? genderId,
-        //                                   string Password, DateTime ? DOJ, string mobileNumber, string ? EmergencycontactNumber, string Designation,
-        //                                   int UserLevelID, int ReportingNextlevel, int User_Group, string EmailID, DateTime ? Dateofbirth, string FatherName, string MotherName
-        //                                  , string SpouseName, int Maritial_ID, string Aadhar_Number, string PAN_Number, string Address, string User_Imagepath, int Is_Active,
-        //                                   int createdBy, int errorCode);
-        //    }
-        //}
+            // here give repository name
+            private readonly IUsermanagementRepository _usermanagementrepository;
+            public Handler(IUsermanagementRepository repository)
+            {
+                _usermanagementrepository = repository;
+            }
+
+            public Task<int> Handle(Command request, CancellationToken cancellationtoken)
+            {
+                int errorcode = 0;
+                //the Datas from UserInsertDetailsModel
+                return _usermanagementrepository.InsertUserDetails(request.UserdetailsCommand.CompanyId.GetValueOrDefault(),
+                    request.UserdetailsCommand.UserID.GetValueOrDefault(),
+                    request.UserdetailsCommand.UserCode,
+                    request.UserdetailsCommand.UserName,
+                    request.UserdetailsCommand.GenderId.GetValueOrDefault(),
+                    request.UserdetailsCommand.Password,
+                    request.UserdetailsCommand.DOJ.GetValueOrDefault(),
+                    request.UserdetailsCommand.MobileNumber,
+                    request.UserdetailsCommand.EmergencycontactNumber,
+                    request.UserdetailsCommand.Designation,
+                    request.UserdetailsCommand.UserLevelID.GetValueOrDefault(),
+                    request.UserdetailsCommand.ReportingNextlevel.GetValueOrDefault(),
+                    request.UserdetailsCommand.UserGroup.GetValueOrDefault(),
+                    request.UserdetailsCommand.EmailID,
+                    request.UserdetailsCommand.Dateofbirth.GetValueOrDefault(),
+                    request.UserdetailsCommand.FatherName,
+                    request.UserdetailsCommand.MotherName,
+                    request.UserdetailsCommand.SpouseName,
+                    request.UserdetailsCommand.MaritialID,
+                    request.UserdetailsCommand.AadharNumber,
+                    request.UserdetailsCommand.PanNumber,
+                    request.UserdetailsCommand.Address,
+                    request.UserdetailsCommand.UserImagepath,
+                    request.UserdetailsCommand.IsActive.GetValueOrDefault(),
+                    request.UserdetailsCommand.CreatedBy.GetValueOrDefault(),
+                    request.UserdetailsCommand.ErrorCode.GetValueOrDefault(),
+                                         );
+            }
+        }
     }
 }
