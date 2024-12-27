17f668460053ff107f635ebd42c14008680f3401
SeshadriSuresh <159230441+SeshadriSuresh@users.noreply.github.com>
2024-12-27 15:02:30 +0530

    Company master api

---
+++ FOS.Infrastructure/FOS.Infrastructure/Queries/GetCompanyMaster.cs
@@ -0,0 +1,47 @@
+﻿using FOS.Models.Entities;
+using FOS.Models.Requests;
+using FOS.Repository.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Infrastructure.Queries
+{
+    public class GetCompanyMaster
+    {
+        public class Query : IRequest<CompanyMasterRequest>
+        {
+            public int? CompanyId { get; set; }
+
+            public Query(int? companyId)
+            {
+
+                CompanyId = companyId;
+
+            }
+        }
+
+
+
+
+        public class Handler : IRequestHandler<Query, CompanyMasterRequest>
+        {
+            private readonly IProspectRepository _getuserrepository;
+
+            public Handler(IProspectRepository repository)
+            {
+                _getuserrepository = repository;
+            }
+            public async Task<CompanyMasterRequest> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _getuserrepository.Get_CompanyMasterRepository(request.CompanyId);
+            }
+        }
+
+      
+    }
+}
