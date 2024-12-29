09c40720e7a8be75e602de536167ec8f59f20201
Srivatsan S <seshadri.srivatsan@gmail.com>
2024-12-29 10:51:23 +0530

    Completed FVR Code

---
+++ FOS.Infrastructure/FOS.Infrastructure/Queries/GetFvrNeighbourHoodDetails.cs
@@ -0,0 +1,22 @@
+﻿using FOS.Models.Entities;
+using FOS.Repository.Interfaces;
+using MediatR;
+
+namespace FOS.Infrastructure.Queries
+{
+    public class GetFvrNeighbourHoodDetails
+    {
+        public class Query(int? userId, int? companyId, int? fieldVerificationId, int? leadId) : IRequest<FvrDetail?>
+        {
+            public int? CompanyId { get; } = companyId;
+            public int? UserId { get; } = userId;
+            public int? FieldVerificationId { get; }=fieldVerificationId;
+            public int? LeadId { get; }=leadId;
+        }
+        public class Handler(IFieldVerficationRepository repository) : IRequestHandler<Query, FvrDetail?>
+        {
+            public Task<FvrDetail?> Handle(Query request, CancellationToken cancellationToken) =>
+                Task.Run(() => repository.GetFvrNeighbourHoodDetails(request.CompanyId, request.UserId, request.LeadId,request.FieldVerificationId));
+        }
+    }
+}
